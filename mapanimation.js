9df0fe303060cc8f74487b516b8f643f6bb1200f
Administrator <admin@example.com>
2023-06-22 19:28:10 -0500

    feat: Se modifico proyecto

--- mapanimation.js
+++ mapanimation.js
@@ -1,3 +1,17 @@
+mapboxgl.accessToken =
+  'pk.eyJ1IjoicG9vbHJpdmVyYSIsImEiOiJjbGl4emU5YXQwNTR2M3JxcjJtbXNxZ3k4In0.pKCIM1I7YnlHzQ1qH9JZwA';
+
+let map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [-71.104081, 42.365554],
+  zoom: 14,
+});
+
+var logoMarket=createMarket();
+
+let marker = new mapboxgl.Marker(logoMarket).setLngLat([-71.092761, 42.357575]).addTo(map);
+
 const busStops = [
   [-71.093729, 42.359244],
   [-71.094915, 42.360175],
@@ -13,22 +27,28 @@ const busStops = [
   [-71.118625, 42.374863],
 ];
 
-mapboxgl.accessToken = 'pk.eyJ1IjoicG9vbHJpdmVyYSIsImEiOiJjbGl4emU5YXQwNTR2M3JxcjJtbXNxZ3k4In0.pKCIM1I7YnlHzQ1qH9JZwA';
-
-let map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11',
-  center: [-71.104081, 42.365554],
-  zoom: 14,
-});
-
-let marker = new mapboxgl.Marker().setLngLat([-71.092761, 42.357575]).addTo(map);
-
 let counter = 0;
+
+function createMarket(){
+  const el = document.createElement('div');
+  const width = 60;
+  const height = 60;
+  el.className = 'marker';
+  // el.style.backgroundImage = `url(car.png)`;
+  el.style.width = `${width}px`;
+  el.style.height = `${height}px`;
+  el.style.backgroundSize = '100%';
+
+  return el;
+}
+
 function move() {
   setTimeout(() => {
     if (counter >= busStops.length) return;
     marker.setLngLat(busStops[counter]);
+    // new mapboxgl.Marker(logoMarket)
+    // .setLngLat(busStops[counter])
+    // .addTo(map);
     counter++;
     move();
   }, 1000);
